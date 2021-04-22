640b6ff7a774ebcd44313b9d0e3e9616c1d208c4
the38th <step_al_ul@mail.ru>
2021-04-22 06:13:49 -0700

    generating models task complete

---
+++ test/factories/sequence.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  sequence(:string, aliases: [:first_name, :last_name, :password, :avatar, :name, :state]) do |n|
+    "string#{n}"
+  end
+
+  sequence(:email) { |n| "person#{n}@example.com" }
+
+  sequence(:type) { ['Developer', 'Manager', 'Admin'][rand(3)] }
+
+  sequence(:description) { |n| "description#{n}" }
+
+  sequence(:expired_at) { Date.tomorrow }
+end
