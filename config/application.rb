9bee8a8d6cde7dbaf678339f8ca7683ae65d24d7
the38th <step_al_ul@mail.ru>
2021-05-11 01:41:12 -0700

    css added

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,7 @@ require_relative 'boot'
 
 require 'rails/all'
 
+config.assets.paths << Rails.root.join('node_modules')
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
