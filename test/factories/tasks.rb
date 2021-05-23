8b73223ada7a01518dd56e47631887969943ccdd
the38th <step_al_ul@mail.ru>
2021-05-23 05:32:01 -0700

    updated factory for tasks

--- test/factories/tasks.rb
+++ test/factories/tasks.rb
@@ -1,10 +1,8 @@
 FactoryBot.define do
   factory :task do
-    name
-    description
-    author factory: :manager
-    assignee factory: :developer
-    state
-    expired_at
+    name { generate :string }
+    description { generate :string }
+    author_id { create :user }
+    assignee_id { create :user }
   end
 end
