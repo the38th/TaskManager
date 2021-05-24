4e015ee906d7b2fd49d38fbaf700371cb11f7f6d
the38th <step_al_ul@mail.ru>
2021-05-24 01:18:26 -0700

    create test failure fixed

--- test/factories/tasks.rb
+++ test/factories/tasks.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :task do
     name { generate :string }
     description { generate :string }
-    author_id { create :user }
-    assignee_id { create :user }
+    author factory: :manager
+    assignee factory: :developer
   end
 end
