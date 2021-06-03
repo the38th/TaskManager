e315466831e7f72c0dc2ecbacecc0f02551892b3
the38th <step_al_ul@mail.ru>
2021-06-03 11:48:00 -0700

    added DESC sort for new_tasks

--- app/controllers/api/v1/application_controller.rb
+++ app/controllers/api/v1/application_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::ApplicationController < Api::ApplicationController
-  RANSACK_DEFAULT_SORT = 'id ASC'.freeze
+  RANSACK_DEFAULT_SORT = 'id DESC'.freeze
 
   def build_meta(collection)
     {
@@ -12,7 +12,7 @@ class Api::V1::ApplicationController < Api::ApplicationController
   end
 
   def ransack_params
-    params.to_unsafe_h.fetch(:q, { s: RANSACK_DEFAULT_SORT })
+    params.to_unsafe_h.fetch(:q).merge( s: RANSACK_DEFAULT_SORT )
   end
 
   def page
