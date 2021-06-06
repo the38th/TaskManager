bd2529d230b9d41cff8e5ccc330e37729331d2f9
the38th <step_al_ul@mail.ru>
2021-06-06 07:33:14 -0700

    TaskPresenter created

---
+++ app/javascript/presenters/TaskPresenter.js
@@ -0,0 +1,14 @@
+import PropTypes from 'prop-types';
+import PropTypesPresenter from 'utils/PropTypesPresenter';
+
+export default new PropTypesPresenter(
+  {
+    id: PropTypes.number,
+    name: PropTypes.string,
+    description: PropTypes.string,
+    state: PropTypes.string,
+    author: PropTypes.object,
+    assignee: PropTypes.object,
+  },
+  {},
+);
