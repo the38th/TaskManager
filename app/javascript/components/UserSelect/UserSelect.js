cfb1db096f410759dec448766408572d148b4120
the38th <step_al_ul@mail.ru>
2021-06-06 06:44:16 -0700

    user props calls changed on UserPresenter calls

--- app/javascript/components/UserSelect/UserSelect.js
+++ app/javascript/components/UserSelect/UserSelect.js
@@ -8,6 +8,7 @@ import FormHelperText from '@material-ui/core/FormHelperText';
 import InputLabel from '@material-ui/core/InputLabel';
 
 import UsersRepository from 'repositories/UsersRepository';
+import UserPresenter from 'presenters/UserPresenter';
 
 import useStyles from './useStyles';
 
@@ -26,8 +27,8 @@ const UserSelect = ({ error, label, isClearable, isDisabled, isRequired, onChang
             cacheOptions
             loadOptions={handleLoadOptions}
             defaultOptions
-            getOptionLabel={(user) => `${user.firstName} ${user.lastName}`}
-            getOptionValue={(user) => user.id}
+            getOptionLabel={(user) => UserPresenter.fullName(user)}
+            getOptionValue={(user) => UserPresenter.id(user)}
             isDisabled={isDisabled}
             isClearable={isClearable}
             defaultValue={value}
@@ -51,7 +52,7 @@ UserSelect.propTypes = {
   isDisabled: PropTypes.bool,
   isRequired: PropTypes.bool,
   onChange: PropTypes.func,
-  value: PropTypes.shape(),
+  value: UserPresenter.shape(),
   helperText: PropTypes.string,
 };
 
