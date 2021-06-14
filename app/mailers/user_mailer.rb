5bd0a7ea69ff9f7eb7bd8fb4880e754d1425bd01
the38th <step_al_ul@mail.ru>
2021-06-14 07:54:19 -0700

    added task in action

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,7 @@
 class UserMailer < ApplicationMailer
     def task_created
       user = params[:user]
+      @task = params[:task]
   
       mail(from: 'noreply@taskmanager.com', to: user.email, subject: 'New Task Created')
     end
