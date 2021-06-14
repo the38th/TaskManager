36cdb995e99a001f18911d80ada09488ef38bf0c
the38th <step_al_ul@mail.ru>
2021-06-14 08:00:05 -0700

    api task controller test created updated

--- test/controllers/api/v1/tasks_controller_test.rb
+++ test/controllers/api/v1/tasks_controller_test.rb
@@ -14,19 +14,23 @@ class Api::V1::TasksControllerTest < ActionController::TestCase
   end
 
   test 'should post create' do
-    author = create(:user)
+    author = create :user
     sign_in(author)
-    assignee = create(:user)
-    task_attributes = attributes_for(:task).
-      merge({ assignee_id: assignee.id })
-    post :create, params: { task: task_attributes, format: :json }
+  
+    assignee = create :user
+    task_attributes = attributes_for(:task).merge({ assignee_id: assignee.id })
+  
+    assert_emails 1 do
+      post :create, params: { task: task_attributes, format: :json }
+    end
     assert_response :created
-
+  
     data = JSON.parse(response.body)
     created_task = Task.find(data['task']['id'])
-
-    assert created_task.present?
-    assert_equal task_attributes.stringify_keys, created_task.slice(*task_attributes.keys)
+  
+    assert { created_task.present? }
+    assert { created_task.assignee == assignee }
+    assert { created_task.author == author }
   end
 
   test 'should put update' do
